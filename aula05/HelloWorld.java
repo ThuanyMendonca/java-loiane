01cf60cea92c1e09214e66c36f33bc6afbbf85b2
ThuanyMendonca <thuany1997@hotmail.com>
2018-05-01 10:56:34 -0300

    Java

---
+++ aula05/HelloWorld.java
@@ -0,0 +1,26 @@
+//Todo nome de classe começa com letra maiuscula
+//com o nome do arquivo
+class HelloWorld {
+	//output (saida)
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+	}
+}
+
+/*	
+	class = declaração da classe
+	HelloWorld = nome da classe
+	
+	############### METODO #################
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+	}
+	#########################################
+	public static = modificadores de acesso do metodo
+	void = não retorna nada
+	main = nome do metodo
+	(String[] args) = parametros/argumentos do metodo
+	System.out.println = saida para o usuário
+	javac Nome_Arquivo.java (compila)
+	java Nome_Arquivo (executa)
+*/
---
+++ aula06/Argumentos.java
@@ -0,0 +1,7 @@
+class Argumentos {
+	//args é um array de strings
+	public static void main(String[] args){
+		//acessar o argumento 0
+		System.out.println("Você digitou: " + args[0]);
+	}
+}
